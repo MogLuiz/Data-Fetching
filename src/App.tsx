7875086b3ca9972f58ab74d0e345f677188a82e1
Luiz Henrique <luizhjramos@outlook.com>
2022-02-14 20:44:47 -0300

    feat: simple API github consuming

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 // Packages
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+type Repository = {
+  full_name: string;
+  description: string;
+};
 
 const App: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/MogLuiz/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
   return <h1>Olá</h1>;
 };
 
