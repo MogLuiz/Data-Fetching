89da96f1ef51d54d86852ec42501d52ba4b93eaa
Luiz Henrique <luizhjramos@outlook.com>
2022-02-15 00:19:30 -0300

    feat: Repos page

---
+++ src/pages/repos.tsx
@@ -0,0 +1,11 @@
+// Packages
+import React from "react";
+
+// Components
+import { RequestWithReactQuery } from "../components";
+
+const Repos: React.FC = () => {
+  return <RequestWithReactQuery />;
+};
+
+export default Repos;
