6f183481a5574df97f5e31b2a94be755b8d162f8
Luiz Henrique <luizhjramos@outlook.com>
2022-02-14 23:45:02 -0300

    feat: API request with React-Query

--- src/components/withReactQuery/index.tsx
+++ src/components/withReactQuery/index.tsx
@@ -1,10 +1,22 @@
 // Packages
+import axios from "axios";
 import React from "react";
 
 // React Query
 import { useQuery } from "react-query";
 
 const WithReactQuery: React.FC = () => {
+  // -------------------------------------------------
+  // useQuery
+  // -------------------------------------------------
+
+  const { data, isFetching } = useQuery("repos", async () => {
+    const response = await axios.get(
+      "https://api.github.com/users/MogLuiz/repos"
+    );
+    return response.data;
+  });
+
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
