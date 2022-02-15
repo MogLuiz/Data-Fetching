1cdcb1810619e80d6b6f2f91a21340773b7fb7f0
Luiz Henrique <luizhjramos@outlook.com>
2022-02-15 00:24:34 -0300

    feat: Pages Folder Patern Architecture

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@
 import React from "react";
 import { Routes } from "react-router-dom";
 
+// Pages
+import { Repo, Repos } from "./pages";
+
 const App: React.FC = () => {
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return (
-    <Routes>
-      
-    </Routes>
-  );
+  return <Routes></Routes>;
 };
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,6 @@
+export { default as Repo } from "./repo";
+export { default as Repos } from "./repos";
+
+// Repo --> Repository By ID
+
+// Repos --> Repositories GetALL
