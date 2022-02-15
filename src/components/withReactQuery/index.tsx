f89ad6e813abcb07048e3094cf539624636c4742
Luiz Henrique <luizhjramos@outlook.com>
2022-02-14 22:33:40 -0300

    feat: initialize WithReactQuery component

---
+++ src/components/withReactQuery/index.tsx
@@ -0,0 +1,14 @@
+// Packages
+import React from "react";
+
+// React Query
+import { useQuery } from "react-query";
+
+const WithReactQuery: React.FC = () => {
+  // -------------------------------------------------
+  // Render
+  // -------------------------------------------------
+  return <div />;
+};
+
+export default WithReactQuery;
