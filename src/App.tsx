87ce2df078d40686daba95bfc403e174641b67be
Luiz Henrique <luizhjramos@outlook.com>
2022-02-14 23:49:06 -0300

    feat: listing repositories on screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,20 @@ import React from "react";
 // Components
 import { RequestWithReactQuery, RequestWithoutReactQuery } from "./components";
 
+// RequestWithReactQuery --> Request using React-Query
+
+// RequestWithoutReactQuery --> Request using the custom Hook
+
 const App: React.FC = () => {
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <RequestWithoutReactQuery />;
+  return (
+    <>
+      {/* <RequestWithoutReactQuery /> */}
+      <RequestWithReactQuery />
+    </>
+  );
 };
 
 export default App;
--- src/components/withReactQuery/index.tsx
+++ src/components/withReactQuery/index.tsx
@@ -5,22 +5,42 @@ import React from "react";
 // React Query
 import { useQuery } from "react-query";
 
+type Repository = {
+  full_name: string;
+  description: string;
+};
+
 const WithReactQuery: React.FC = () => {
   // -------------------------------------------------
   // useQuery
   // -------------------------------------------------
 
-  const { data, isFetching } = useQuery("repos", async () => {
-    const response = await axios.get(
-      "https://api.github.com/users/MogLuiz/repos"
-    );
-    return response.data;
-  });
+  const { data: repositories, isFetching } = useQuery<Repository[]>(
+    "repos",
+    async () => {
+      const response = await axios.get(
+        "https://api.github.com/users/MogLuiz/repos"
+      );
+      return response.data;
+    }
+  );
 
   // -------------------------------------------------
   // Render
   // -------------------------------------------------
-  return <div />;
+  return (
+    <ul>
+      {isFetching && <p>Loading...</p>}
+      {repositories?.map((repo) => {
+        return (
+          <li key={repo.full_name}>
+            <strong>{repo.full_name}</strong>
+            <p>{repo.description}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default WithReactQuery;
